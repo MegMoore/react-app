a42e3769365e726e9c6483d2bda091114e05eed4
MegMoore <136514941+MegMoore@users.noreply.github.com>
2024-01-01 12:38:03 -0500

    init

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+
+//PascalCasing -- Capitalizing first letter of every word (P,C)
+function Message() {
+    // JSX: JavaScript XML
+    const name = '';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>;
+}
+
+export default Message;
