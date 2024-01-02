46c9b1a6952135de25d9436ff33a127268914191
MegMoore <136514941+MegMoore@users.noreply.github.com>
2024-01-02 13:25:01 -0500

    tried out different techniques

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
 import { useState } from "react";
 import Alert from "./components/Alert";
-import Button from "./components/Button";
+import Button from "./components/Button/Button";
 import ListGroup from "./components/ListGroup";
 import "./App.css";
 
+import { FaCalendar } from "react-icons/fa";
+import Like from "./components/Like";
+
 function App() {
   const items = ["New York", "Los Angeles", "San Francisco"];
-  // const [alertVisible, setAlertVisibility] = useState(false);
+  const [alertVisible, setAlertVisibility] = useState(true);
 
   return (
     <div>
+      <Like onClick={() => console.log("clicked")} />
+      <FaCalendar color="red" size={40} />
       <ListGroup
         items={items}
         heading="Miami"
         onSelectItem={() => items}
       ></ListGroup>
 
-      {/*{alertVisible && (
+      {alertVisible && (
         <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
       )}
       <Button color="secondary" onClick={() => setAlertVisibility(true)}>
         My Button
-      </Button>*/}
+      </Button>
     </div>
   );
 }
---
+++ src/components/Like.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+
+interface Props {
+  onClick: () => void;
+}
+
+const Like = ({ onClick }: Props) => {
+  const [status, setStatus] = useState(false);
+
+  const toggle = () => {
+    setStatus(!status);
+    onClick();
+  };
+  if (status) return;
+  <FaHeart color="#ff6b81" size={50} onClick={toggle} />;
+  return <FaRegHeart size={50} onClick={toggle} />;
+};
+
+export default Like;
