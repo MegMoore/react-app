d66b90cfe3cf964d4981671cc37ce0e54aedcd96
MegMoore <136514941+MegMoore@users.noreply.github.com>
2024-01-02 12:24:34 -0500

    practicing css

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
 import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
+import "./App.css";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
+  const items = ["New York", "Los Angeles", "San Francisco"];
+  // const [alertVisible, setAlertVisibility] = useState(false);
 
   return (
     <div>
-      {alertVisible && (
+      <ListGroup
+        items={items}
+        heading="Miami"
+        onSelectItem={() => items}
+      ></ListGroup>
+
+      {/*{alertVisible && (
         <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
       )}
       <Button color="secondary" onClick={() => setAlertVisibility(true)}>
         My Button
-      </Button>
+      </Button>*/}
     </div>
   );
 }
